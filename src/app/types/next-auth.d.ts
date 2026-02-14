c47166da9306f9a55747aa780f36f4afe4264662
engAhmedAli901 <hgdvdbdbd00@gmail.com>
2026-02-14 18:39:04 +0200

    Update next-auth.d.ts

--- src/app/types/next-auth.d.ts
+++ src/app/types/next-auth.d.ts
@@ -1,22 +1,21 @@
+import { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
 import { UserResponse } from "@/interfaces"
-import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: UserResponse
+    accessToken: string
   }
-  interface User{
-    user : UserResponse , 
-    token : string
+
+  interface User extends UserResponse {
+    accessToken: string
   }
 }
 
-import { JWT } from "next-auth/jwt"
-
 declare module "next-auth/jwt" {
-  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
-  interface JWT extends User{}
+  interface JWT {
+    user: UserResponse
+    accessToken: string
+  }
 }
