ab7e74c34d79c9a9638a132eb7f3092771ad0d72
engAhmedAli901 <hgdvdbdbd00@gmail.com>
2026-02-14 19:01:47 +0200

    Update route.ts

--- src/api/auth/[...nextauth]/route.ts
+++ src/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,63 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { UserResponse } from "@/interfaces"
 
 const handler = NextAuth({
-  providers: [
+    session: {
+    strategy: "jwt"
+  },
+  providers:[
     CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials) {
-        const res = await fetch("https://ecommerce.routemisr.com/api/v1/auth/signin", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(credentials),
-        })
-
-        const data = await res.json()
-
-        if (!res.ok) {
-          throw new Error(data.message || "Login failed")
-        }
-
-        // 👇 هنا أهم سطر
-        return {
-          ...data.user,
-          accessToken: data.token,
-        } as any   // ← مهم عشان TypeScript
-      },
+         name: 'Credentials',
+         credentials:{
+            email:{},
+            password:{}
+         },
+       async authorize(credentials) {
+  const res = await fetch(`https://ecommerce.routemisr.com/api/v1/auth/signin`, {
+    method: "POST",
+    body: JSON.stringify({
+      email: credentials?.email,
+      password: credentials?.password
     }),
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+
+  const payload = await res.json()
+
+  if (payload.message !== "success") {
+    return null
+  }
+
+  return {
+    id: payload.user._id,
+    email: payload.user.email,
+    name: payload.user.name,
+    accessToken: payload.token     
+  }
+}
+
+    })
   ],
-
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.user = user as UserResponse
-        token.accessToken = (user as any).accessToken
-      }
-      return token
+  callbacks:{
+    jwt: ({user , token})=>{
+        if(user){
+            token.user = user.user
+        token.token = user.token
+        }
+        return token
     },
-
-    async session({ session, token }) {
-      session.user = token.user as UserResponse
-      session.accessToken = token.accessToken as string
-      return session
-    },
+    session : ({session , token})=>{
+        session.user = token.user
+        return session
+    }
   },
-
-  pages: {
-    signIn: "/login",
-    error: "/login", // يخليه يرجع لنفس صفحة اللوجين بدل api/auth/error
+  pages:{
+    signIn : "/login"
   },
-
-  session: {
-    strategy: "jwt",
-  },
-
-  secret: process.env.NEXTAUTH_SECRET,
+  secret:process.env.AUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
+
